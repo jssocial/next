7537639827e666e38b058c880b7ac2e0de328566
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2021-01-14 16:49:17 +0300

    new untracked files are committed

---
+++ components/Nav.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useContext } from "react";
+import Link from "next/link";
+
+
+export default function Nav() {
+return (
+<div>
+  <div className="side-bar"></div>
+      <header>
+        <div className="header-sec">
+          <div className="container">
+            <div className="header-sec-in">
+              <div className="logo">
+                <Link href="index.html"
+                  ><img
+                    src="images/logo.png"
+                    alt="logo"
+                    width="156"
+                    height="75"
+                /></Link>
+              </div>
+              <button type="button" className="menu-icon">
+                <span></span>
+                <span></span>
+                <span></span>
+              </button>
+              <div className="header-right">
+                <nav>
+                  <ul className="clearfix">
+                    <li><Link href="about.html">About</Link></li>
+                    <li><Link href="mentors.html">Mentors</Link></li>
+                    <li><Link href="programs.html">Programs</Link></li>
+                    <li><Link href="membership.html"> Membership </Link></li>
+                  </ul>
+                </nav>
+                <div className="login">
+                  <Link href="https://discord.gg/yDjJhmx" target="_blank" >Login</Link>
+                </div>
+                <div className="clear"></div>
+              </div>
+              <div className="clear"></div>
+            </div>
+          </div>
+        </div>
+      </header>
+      </div>
+)}
